e9800c17e31b5400653e9096349c414e39d221cc
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-03-01 09:55:37 -0300

    frontend: colocando o PhoneBookContext e usePhoneBook em um arquivo so

--- frontend/src/hooks/usePhoneBook.js
+++ frontend/src/hooks/usePhoneBook.js
@@ -1,9 +1,70 @@
-import { useContext } from 'react';
-import { PhoneBookContext } from './PhoneBookContext';
+import { createContext, useContext, useEffect, useState } from 'react';
+import mockPhoneBook from '../mockPhoneBook.js';
+
+export const PhoneBookContext = createContext()
+
+export default function PhoneBookProvider({ children }) {
+   const [phoneBook, setPhoneBook] = useState([]);
+
+   useEffect(() => {
+      (async () => {
+         const findAll = async () => {
+            const url = process.env.REACT_APP_BACKEND_URL;
+            const res = await fetch(`${url}/contact`, { method: "GET" });
+            return await res.json()
+         }
+
+         const { contacts } = await findAll()
+
+         setPhoneBook([...contacts])
+         // setPhoneBook([...mockPhoneBook])
+      })()
+   }, []);
+
+   function editContactInContext(contactToEditState) {
+      const editedContact = {
+         ...contactToEditState
+      }
+
+      const newPhoneBook = phoneBook.filter(contact => contact.id !== editedContact.id)
+
+      setPhoneBook([
+         editedContact,
+         ...newPhoneBook,
+      ])
+   }
+
+   function createContactInContext(contact) {
+      delete contact.createdAt
+      delete contact.updatedAt
+
+      setPhoneBook([
+         contact,
+         ...phoneBook
+      ])
+   }
+
+   function removeContactInContext(id) {
+      const newPhoneBook = phoneBook.filter(contact => contact.id !== id)
+      setPhoneBook([...newPhoneBook])
+   }
+
+   function findContactInContext(id) {
+      return phoneBook.find(contact => contact.id === id)
+   }
+
+   return <PhoneBookContext.Provider value={{
+      editContactInContext,
+      removeContactInContext,
+      findContactInContext,
+      createContactInContext,
+      phoneBook,
+      setPhoneBook
+   }} >{children}</PhoneBookContext.Provider>
+}
 
 export function usePhoneBook() {
    const context = useContext(PhoneBookContext)
-
    const {
       editContactInContext,
       createContactInContext,
