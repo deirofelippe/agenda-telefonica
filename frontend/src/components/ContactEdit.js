98bb559268fe198446dd05736105cd30a17321ad
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-03-01 08:43:50 -0300

    frontend: ContactEdit com find e edit do contextApi, modifica imagem no css, preview de imagem e put e delete no S3

--- frontend/src/components/ContactEdit.js
+++ frontend/src/components/ContactEdit.js
@@ -1,27 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams, useHistory } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { usePhoneBook } from '../hooks/usePhoneBook';
-import './ContactFormEdit.css'
+import { s3DeleteFile, s3PutFile } from '../s3';
+import './ContactEdit.css'
+import defaultImage from './default_avatar.svg'
 
 const ContactEdit = () => {
    const { id } = useParams()
 
-   const history = useHistory()
+   const { findContactInContext, editContactInContext } = usePhoneBook()
 
-   const { findContact, editContact } = usePhoneBook()
+   const [imageStatus, setImageStatus] = useState({
+      oldFileName: '',
+      imagePreview: '',
+      removeImageFromS3: false,
+      file: {}
+   });
 
    const [contactToEdit, setContactToEdit] = useState({
+      id: id,
       name: '',
       email: '',
       phone: '',
+      image: ''
    });
 
    useEffect(() => {
-      const contactFound = findContact(id)
-      setContactToEdit(contactFound)
-   }, [findContact, id]);
+      const contactFound = findContactInContext(id)
 
-   const change = (event) => {
+      setContactToEdit({
+         ...contactFound,
+      })
+
+      const imageExist = !!contactFound.image
+      const preview = imageExist ? getImageURL(contactFound.image) : defaultImage
+
+      setImageStatus({
+         ...imageStatus,
+         oldFileName: contactFound.image,
+         imagePreview: preview
+      })
+   }, []);
+
+   const getImageURL = (image) => {
+      if (image) {
+         const bucket = process.env.REACT_APP_AWS_BUCKET_NAME
+         const region = process.env.REACT_APP_AWS_REGION
+         return `https://${bucket}.s3.${region}.amazonaws.com/${image}`
+      }
+
+      return defaultImage
+   }
+
+   const inputChange = (event) => {
       const { name, value } = event.target;
 
       setContactToEdit({
@@ -30,9 +61,78 @@ const ContactEdit = () => {
       });
    };
 
+   const fileChange = (event) => {
+      const file = event.target.files[0]
+
+      let preview = defaultImage
+      let imageName = ''
+      let removeImageFromS3 = true
+
+      if (file) {
+         preview = URL.createObjectURL(file)
+         imageName = getImageName(file.name)
+         removeImageFromS3 = false
+      }
+
+      console.log(file, preview);
+
+      setImageStatus({
+         ...imageStatus,
+         imagePreview: preview,
+         removeImageFromS3,
+         file: file || {}
+      })
+
+      setContactToEdit({
+         ...contactToEdit,
+         image: imageName
+      })
+   }
+
+   const removeImage = (event) => {
+      event.preventDefault()
+
+      setImageStatus({
+         ...imageStatus,
+         removeImageFromS3: true,
+         imagePreview: defaultImage,
+         file: {}
+      })
+
+      setContactToEdit({
+         ...contactToEdit,
+         image: ''
+      })
+   }
+
+   const getImageName = (filename) => {
+      const { id } = contactToEdit
+
+      const imageNameArray = filename.split('.')
+      const lastIndex = imageNameArray.length - 1
+      const ext = imageNameArray[lastIndex]
+      const imageName = `${id}.${ext}`
+
+      return imageName
+   }
+
    const edit = async (event) => {
       event.preventDefault();
 
+      const res = await sendRequest()
+
+      if (!res.ok) {
+         return;
+      }
+
+      await fileUpload()
+
+      editContactInContext(contactToEdit)
+
+      initialState()
+   };
+
+   const sendRequest = async () => {
       const url = process.env.REACT_APP_BACKEND_URL;
 
       const res = await fetch(`${url}/contact/${id}`, {
@@ -40,19 +140,41 @@ const ContactEdit = () => {
          headers: {
             "Content-Type": "application/json",
          },
-         body: JSON.stringify({ ...contactToEdit }),
+         body: JSON.stringify({
+            ...contactToEdit
+         }),
       });
 
-      await res.json();
+      console.log('res');
 
-      if (!res.ok) {
-         return
+      return res
+   }
+
+   const fileUpload = async () => {
+      const { file, oldFileName, removeImageFromS3 } = imageStatus
+      const { image: newFileName } = contactToEdit
+
+      if (oldFileName && file?.name) {
+         await s3DeleteFile(oldFileName)
+         await s3PutFile(file, newFileName)
+      } else if (removeImageFromS3 && oldFileName) {
+         await s3DeleteFile(oldFileName)
+      } else if (!oldFileName && file?.name) {
+         await s3PutFile(file, newFileName)
       }
+   }
 
-      editContact(contactToEdit)
+   const initialState = () => {
+      const { image } = contactToEdit
+      const preview = image ? getImageURL(image) : defaultImage
 
-      history.push('/contact')
-   };
+      setImageStatus({
+         removeImageFromS3: false,
+         imagePreview: preview,
+         oldFileName: image,
+         file: {},
+      })
+   }
 
    return (
       <>
@@ -70,12 +192,24 @@ const ContactEdit = () => {
          <main>
             <section className='section'>
                <form action="#">
+                  <div className="form-group remove-image">
+                     <button onClick={removeImage}>Remover imagem</button>
+                  </div>
+                  <div className="form-group edit-image">
+                     <img className='img-preview' src={imageStatus.imagePreview} alt="" />
+                     <div>
+                        <label htmlFor="image">Imagem</label>
+                        <input type="file"
+                           id="image"
+                           onChange={fileChange} />
+                     </div>
+                  </div>
                   <div className="form-group">
                      <label htmlFor="name">Nome</label>
                      <input id="name"
                         name="name"
                         type="text"
-                        onChange={change}
+                        onChange={inputChange}
                         value={contactToEdit?.name}
                         placeholder="Seu nome" />
                   </div>
@@ -84,7 +218,7 @@ const ContactEdit = () => {
                      <input id="phone"
                         name="phone"
                         type="text"
-                        onChange={change}
+                        onChange={inputChange}
                         value={contactToEdit?.phone}
                         placeholder="91234-5678" />
                   </div>
@@ -93,7 +227,7 @@ const ContactEdit = () => {
                      <input id="email"
                         name="email"
                         type="text"
-                        onChange={change}
+                        onChange={inputChange}
                         value={contactToEdit?.email}
                         placeholder="Seu email" />
                   </div>
