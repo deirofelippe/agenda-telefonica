c889adc7ac04d1ac3fc2121356ae4b319e2cdc06
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-15 12:35:12 -0300

    backend/feat(controller): adiciona type com jsdocs no images

--- backend/src/controller/images.controller.js
+++ backend/src/controller/images.controller.js
@@ -1,8 +1,19 @@
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
+import "../types/index.js";
+
+/**
+ * @param {RequestExpress} req
+ * @param {ResponseExpress} res
+ * @returns {ResponseExpress}
+ */
 async function preSignedUrl(req, res) {
   try {
+    /** @type {{ imageName: string }} */
+    const body = req.body;
+
+    /** @type {DeleteObjectCommandInput} */
     const clientParams = {
       region: "sa-east-1",
       forcePathStyle: true,
@@ -15,8 +26,9 @@ async function preSignedUrl(req, res) {
 
     const client = new S3Client(clientParams);
 
-    const imageName = req.body.imageName;
+    const imageName = body.imageName;
 
+    /** @type {PutObjectCommandInput} */
     const putObjectParams = {
       Bucket: "agenda-images",
       Key: imageName,
@@ -33,6 +45,4 @@ async function preSignedUrl(req, res) {
   }
 }
 
-module.exports = {
-  preSignedUrl,
-};
+export { preSignedUrl };
