8049aeaaf6805c33a210b5e40cfd15d0e45ecb6c
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-15 11:02:54 -0300

    backend/feat(server): tira dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,16 @@
-require("dotenv").config();
-
 const app = require("./app");
 const db = require("./database");
 
 const PORT = process.env.API_PORT || 3000;
 
 (async () => {
-   try {
-      await db.sync();
-      console.log("Database sync...");
+  try {
+    await db.sync();
+    console.log("Database sync...");
 
-      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-   } catch (error) {
-      console.log(error);
-      await db.close();
-   }
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+    await db.close();
+  }
 })();
