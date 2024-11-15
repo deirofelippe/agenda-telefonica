e5afcffe65480da57bf4033f645adb4d4caacb2a
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-15 12:37:23 -0300

    backend/feat(types): cria arquivos de types

---
+++ backend/src/types/index.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {Object} Contact
+ * @property {string} id
+ * @property {string} name
+ * @property {string} email
+ * @property {string} phone
+ * @property {string} image
+ * @property {string} createdAt
+ * @property {string} updatedAt
+ */
+
+/** @typedef {import("@aws-sdk/client-s3").DeleteObjectCommandInput} DeleteObjectCommandInput */
+/** @typedef {import("@aws-sdk/client-s3").PutObjectCommandInput} PutObjectCommandInput */
+
+/** @typedef {import("express").Request} RequestExpress */
+/** @typedef {import("express").Response} ResponseExpress */
