bdc34069ec6733e3dcc732694137e20afafa5f30
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-15 15:00:32 -0300

    frontend/feat: cria ContactContext e ContactProvider

---
+++ frontend/src/context/ContactContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from "react"
+import { ContactProps } from "../App"
+
+export type ContactContextType = {
+   createContact: (contact: ContactProps) => void
+   editContact: (contact: ContactProps) => void
+   removeContact: (id: string) => void
+   findContact: (id: string) => ContactProps | undefined
+   contacts: ContactProps[]
+}
+
+const ContactContext = createContext<Partial<ContactContextType>>({
+   contacts: [],
+})
+
+export default ContactContext
---
+++ frontend/src/context/ContactProvider.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useReducer } from "react"
+import { ContactApi, ContactProps } from "../App"
+import appReduce from "./AppReduce"
+import { env } from "../env"
+import ContactContext from "./ContactContext"
+
+const initialState: { contacts: ContactProps[] } = {
+   contacts: [],
+}
+
+interface ContactProviderProps {
+   children: React.ReactNode
+}
+
+const ContactProvider = ({ children }: ContactProviderProps) => {
+   const [state, dispatch] = useReducer(appReduce, initialState)
+
+   useEffect(() => {
+      ;(async () => {
+         const findAll = async (): Promise<ContactApi[]> => {
+            const url = env.backendUrl
+
+            const res = await fetch(`${url}/contacts`, { method: "GET" })
+
+            const response = await res.json()
+
+            return response.contacts
+         }
+
+         const contactsFound = await findAll()
+
+         const contacts = contactsFound.map(
+            (contact): ContactProps => ({
+               id: contact.id,
+               email: contact.email,
+               name: contact.name,
+               phone: contact.phone,
+               createdAt: contact.createdAt,
+               updatedAt: contact.updatedAt,
+               imageName: contact.image,
+               imageContent: undefined,
+            })
+         )
+
+         dispatch({
+            type: "CREATE_ALL",
+            payload: { contacts },
+         })
+      })()
+   }, [])
+
+   function createContact(contact: ContactProps) {
+      dispatch({
+         type: "CREATE",
+         payload: { contact },
+      })
+   }
+
+   function editContact(contact: ContactProps) {
+      dispatch({
+         type: "EDIT",
+         payload: { contact },
+      })
+   }
+
+   function removeContact(id: string) {
+      dispatch({
+         type: "REMOVE",
+         payload: { id },
+      })
+   }
+
+   function findContact(id: string) {
+      return state.contacts.find((contact) => contact.id === id)
+   }
+
+   const context = {
+      createContact,
+      findContact,
+      editContact,
+      removeContact,
+      contacts: state.contacts,
+   }
+
+   return (
+      <ContactContext.Provider value={context}>
+         {children}
+      </ContactContext.Provider>
+   )
+}
+
+export default ContactProvider
