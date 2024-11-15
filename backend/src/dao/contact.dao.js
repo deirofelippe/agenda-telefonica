f5a0d549f61c768d47fdb1bdc78ef301bd20aa7c
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-15 11:06:30 -0300

    backend/feat(dao): melhora o log de error

--- backend/src/dao/contact.dao.js
+++ backend/src/dao/contact.dao.js
@@ -1,70 +1,72 @@
 const model = require("../models/contact.js");
 
-const cleanOutput = (list) =>
-   list.map(item => item?.dataValues)
+const cleanOutput = (list) => list.map((item) => item?.dataValues);
 
 async function findAll() {
-   try {
-      const result = await model.findAll({
-         order: [
-            ['name', 'ASC']
-         ]
-      });
+  try {
+    const result = await model.findAll({
+      order: [["name", "ASC"]],
+    });
 
-      const cleanResult = cleanOutput(result)
+    const cleanResult = cleanOutput(result);
 
-      return cleanResult
-   } catch (error) {
-      console.log("[BD ERROR] " + error);
-      throw 'error'
-   }
+    return cleanResult;
+  } catch (error) {
+    console.error("[BD ERROR] " + error);
+    console.error(error);
+    throw "error";
+  }
 }
 
 async function create(contact) {
-   try {
-      const result = await model.create(contact);
+  try {
+    const result = await model.create(contact);
 
-      return result.dataValues;
-   } catch (error) {
-      console.log("[BD ERROR] " + error);
-      throw 'error'
-   }
+    return result.dataValues;
+  } catch (error) {
+    console.error("[BD ERROR] " + error);
+    console.error(error);
+    throw "error";
+  }
 }
 
 async function findById(id) {
-   try {
-      const result = await model.findOne({ where: { id } });
+  try {
+    const result = await model.findOne({ where: { id } });
 
-      return result?.dataValues;
-   } catch (error) {
-      console.log("[BD ERROR] " + error);
-      throw 'error'
-   }
+    return result?.dataValues;
+  } catch (error) {
+    console.error("[BD ERROR] " + error);
+    console.error(error);
+    throw "error";
+  }
 }
 
 async function update(newContact) {
-   try {
-      const { id } = newContact;
-      await model.update(newContact, { where: { id } });
-   } catch (error) {
-      console.log("[BD ERROR] " + error);
-      throw 'error'
-   }
+  try {
+    const { id } = newContact;
+    await model.update(newContact, { where: { id } });
+  } catch (error) {
+    console.error("[BD ERROR] " + error);
+    console.error(error);
+    throw "error";
+  }
 }
 
 async function remove(id) {
-   try {
-      await model.destroy({ where: { id } });
-   } catch (error) {
-      console.log("[BD ERROR] " + error);
-      throw 'error'
-   }
+  try {
+    await model.destroy({ where: { id } });
+  } catch (error) {
+    console.error("[BD ERROR] " + error);
+    console.error(error);
+    throw "error";
+  }
 }
 
 module.exports = {
-   remove,
-   update,
-   findById,
-   findAll,
-   create,
+  remove,
+  update,
+  findById,
+  findAll,
+  create,
 };
