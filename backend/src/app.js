706a128aa6a622f112e9baede0ea5433983ffd80
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-16 23:20:49 -0300

    backend/config(app): adiciona funcao de error handler no express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import "./types/index.js";
 
 const app = express();
 
@@ -8,5 +9,18 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(errorHandler);
+
+/**
+ * @param {RequestExpress} req
+ * @param {ResponseExpress} res
+ * @param {import("express").NextFunction} next
+ * @param {import("express").ErrorRequestHandler} error
+ */
+function errorHandler(req, res, next, error) {
+  console.log(error);
+
+  return res.status(500).json({ message: "Internal Server Error" });
+}
 
 export default app;
