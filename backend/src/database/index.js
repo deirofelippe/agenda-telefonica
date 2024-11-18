52043cd09851330e3efdaba74334a3d88e2573c8
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-18 19:10:27 -0300

    backend/config(db): cria função para criar banco de producao e de teste

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,29 @@
 import { Sequelize } from "sequelize";
 import Contact from "../models/contact.js";
 import { env } from "../env.js";
+import mysql from "mysql2/promise";
+
+async function createDatabasesIfNotExists() {
+  const dbConnection = await mysql.createConnection({
+    host: env.dbConfig.host,
+    user: env.dbConfig.username,
+    password: env.dbConfig.password,
+  });
+
+  await dbConnection.query(
+    `CREATE DATABASE IF NOT EXISTS ${env.dbConfig.database}`
+  );
+
+  if (env.isTesting) {
+    await dbConnection.query(
+      `CREATE DATABASE IF NOT EXISTS ${env.dbConfig.database}_test`
+    );
+  }
+
+  await dbConnection.end();
+}
+
+await createDatabasesIfNotExists();
 
 const connection = new Sequelize(env.dbConfig);
 
