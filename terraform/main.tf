983df20c9c17ac9843a533a4cd8dd10e2445e60e
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-30 20:13:03 -0300

    config(terraform): adiciona backend para producao

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,21 +6,14 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "agenda-remote-state"
-    key            = "dev/terraform.tfstate"
-    region         = "sa-east-1"
-    use_path_style = true
-
-    endpoints = {
-      s3  = "http://localhost:4566"
-      sts = "http://localhost:4566"
-    }
-  }
+  # backend "s3" {
+  #   bucket = "agenda-remote-state"
+  #   key    = "production/terraform.tfstate"
+  #   region = "sa-east-1"
+  # }
 }
 
 provider "aws" {
   shared_credentials_files = [var.aws_credentials_file_path]
   region                   = var.aws_region
-
 }
