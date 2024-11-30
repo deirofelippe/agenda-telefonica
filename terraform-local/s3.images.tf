99a4492f165816e9c4f55429894976cc5d1a1c05
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-30 20:22:12 -0300

    config(terraform-local): adiciona permissões para mudança nos objetos no s3 e cors do frontend

--- terraform-local/s3.images.tf
+++ terraform-local/s3.images.tf
@@ -6,13 +6,33 @@ resource "aws_s3_bucket" "images" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_cors_configuration" "images" {
   bucket = aws_s3_bucket.images.id
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["http://localhost:3001", "http://agenda-frontend:3001", "http://localhost:3000", "http://agenda-backend:3000"]
+    allowed_methods = ["PUT", "POST", "DELETE"]
+    allowed_origins = [
+      "http://localhost:3001",
+      "http://agenda-frontend:3001",
+    "http://localhost:3000", "http://agenda-backend:3000"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
