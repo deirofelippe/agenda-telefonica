3af5f5f58913ef62eaa9274baeb62091e57503f5
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-30 20:09:13 -0300

    config(terraform): atualiza path do ec2 key e atribui role para o ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "this" {
   key_name   = "agenda_ec2_key"
-  public_key = file("./agenda_ec2_key.pub")
+  public_key = file("../agenda_ec2_key.pub")
 
   tags = {
     Name = "Agenda Key Pair"
@@ -8,12 +8,14 @@ resource "aws_key_pair" "this" {
 }
 
 resource "aws_instance" "this" {
+  depends_on = [aws_internet_gateway.this]
+
   ami                    = "ami-0f16d0d3ac759edfa"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.this.key_name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.api.id]
-  depends_on             = [aws_internet_gateway.this]
+  iam_instance_profile   = aws_iam_instance_profile.s3_to_ec2.name
 
   tags = {
     Name = "Agenda EC2"
