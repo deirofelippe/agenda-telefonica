5b18a7756af382a0528dc46517385391117473c0
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-11-30 19:42:44 -0300

    frontend/config(eslint): adiciona warning

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,6 +20,9 @@ export default tseslint.config(
       rules: {
          ...reactHooks.configs.recommended.rules,
          "@typescript-eslint/no-unused-vars": ["warn"],
+         "@typescript-eslint/no-explicit-any": ["warn"],
+         "@typescript-eslint/ban-ts-comment": ["warn"],
+         "prefer-const": ["warn"],
          "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
